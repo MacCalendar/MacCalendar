0b618a6d5a3b49e3fcd0f9726c62196b1df9fd17
Mikhail Andrenkov <M.Andrenkov@gmail.com>
2014-11-14 18:50:07 -0500

    Add ClassTime
    Stores information about a class time

---
+++ ClassTime.java
@@ -0,0 +1,10 @@
+package macCalendar;
+
+/**
+ * @author Mikhail Andrenkov
+ * Stores information about a class time
+ */
+public class ClassTime {
+
+	public static String dept, code, comp, term, day, start, end, loc;
+}
